c6aa2d97b1cab314297d056aa2929e625f540498
Sahil Gurav <sahilgurav3005@gmail.com>
2025-06-13 16:57:44 +0530

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
@@ -7,12 +7,29 @@ import Courses from './pages/Courses';
 import Contact from './pages/Contact';
 import Apply from './pages/Apply';
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 import ChatbotComponent from './components/Chatbot/ChatbotComponents';
+import DeveloperInfoPopup from './components/DeveloperInfo/DeveloperInfoPopup';
 
 const App = () => {
+  const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
   return (
     <>
-      <Navbar />
+    <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Sahil Samir Gurav"
+          studentPhotoUrl="/sahil.jpeg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -22,6 +39,8 @@ const App = () => {
         
       </Routes>
       <ChatbotComponent></ChatbotComponent>
+      <Footer/>
+
     </>
   );
 };
